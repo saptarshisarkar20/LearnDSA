6be3dec2a44c841e05474926c760a60055c7ca72
Void23 <96782328+Subhojit666@users.noreply.github.com>
2022-10-11 19:53:28 +0530

    palNo.c

---
+++ solve here/LeetCode/PalindromeNumber/palNo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int is_palindrome(int _num) {
+    if(_num < 10 || ((_num % 10 == 0) && _num != 0)) return 0;
+
+    int rev = 0;
+    int rem = 0;
+
+    while(_num != 0) {
+        rem = _num % 10;
+        rev *= 10 + rem;
+        _num /= 10;
+    }
+
+    return (_num == rev);
+}
+
+int main(int _argc, char** _argv) {
+    int raw;
+    printf("Enter an integer: ");
+    scanf("%d", &raw);
+    printf("%d\n", is_palindrome(raw));
+    return 0;
+}
